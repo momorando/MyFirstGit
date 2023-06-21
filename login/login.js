f3855c63a0406bccbf90485aff0ac17fbcb9ad37
git <momorance@gmail.com>
2023-06-21 16:26:59 +0900

    login.js logout함수추가

---
+++ login/login.js
@@ -0,0 +1,14 @@
+//login을 처리하는 js 파일
+
+
+function login(username,password, flag){
+    if(username !== "" && password !== null && flag){
+        return true
+
+    }
+    return false
+}
+
+function logout() {
+    return true;
+}
